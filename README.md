15848206387b80092e8b907a75e61b0eb6428297
davetron33 <dave@iamdave.wtf>
2023-03-25 21:45:50 -0500

    Dealing with null values

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # pyxfl
 
-Is this officially version 2? It's a pretty substantial change. We have classes now! And a lot less crap in the repo.
+Is this officially version 2? It's a pretty substantial change. We have classes now! And a lot less crap in the repo. 
 
 
 # FAQ
 
 **What is this?**
+
 An attempt to create a library to query the XFL stats API using python. 
 
 **Why are you doing this?**
+
 Why indeed.
 
 **How do I contribute?**
+
 Gitops. Write code. Open PR. 
 
 **Who are you?**
+
 Space ghost.
--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class xfl:
         'access_token': f'{token}'
         }
 
-    def __init__(self, players_endpoint, token):
+    def __init__(self, token):
         self.url = players_endpoint
         self.token = token
         pass
@@ -25,19 +25,18 @@ class xfl:
 
         try:
             r = requests.get(url, params=api_params)
-
             r.raise_for_status()
 
             api_response = json.dumps(r.text, indent=4, sort_keys=True)
             response = json.loads(api_response)
+
+            global players
             players = pd.read_json(response)
 
             print(players)
             pass
-
         except requests.exceptions.HTTPError as err:
             raise SystemExit(err)
-
 
     def get_stats_combined(self, scope=""):
         url = stats_endpoint
@@ -48,16 +47,16 @@ class xfl:
 
         try:
             r = requests.get(url, params=api_params)
-
             r.raise_for_status()
 
             api_response = json.dumps(r.text, indent=4, sort_keys=True)
             response = json.loads(api_response)
-            player_stats = pd.read_json(response)
+
+            global player_stats
+            player_stats = pd.read_json(response).fillna(0)
 
             print(player_stats)
             pass
-
         except requests.exceptions.HTTPError as err:
             raise SystemExit(err)
 
@@ -65,12 +64,12 @@ class xfl:
 
 
 # create a new instance of the xfl-api object
-xfl = xfl(players_endpoint, token)
+xfl = xfl(token)
 
 # list all players in the XFL (returns as a pandas data frame)
-players  = xfl.list_players()
+players = xfl.list_players()
 print(players)
 
 # get combined season stats stats
-player_stats = p.get_stats_combined("season")
+player_stats = xfl.get_stats_combined("season")
 print(player_stats)
