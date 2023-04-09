fde1aeb3f3da92eab2eb07b48cca17e73aa80b5a
davetron33 <dave@iamdave.wtf>
2023-04-08 22:11:12 -0500

    v1.2 todos, postgresql support and get_games

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import pandas as pd
+from sqlalchemy import create_engine
 import requests
-import argparse
 import json
 import os
 
+# initialize sqlite3 connection
+
 # initialize a few happy variables. variables are cool.
 token = os.getenv('access_token')
-players_endpoint = 'https://api.xfl.com/scoring/v3.30/players'
-teams_endpoint = 'https://api.xfl.com/scoring/v3.30/teams'
-stats_endpoint = 'https://api.xfl.com/scoring/v3.30/playerstats'
+
+psql_host = os.getenv('psql_host')
+psql_user = os.getenv('psql_user')
+psql_pass = os.getenv('psql_pass')
+psql_data = 'pyxfl'
+
+engine = create_engine(f'postgresql://{psql_user}:{psql_pass}@{psql_host}/{psql_data}')
+
+# TODO: Instead use a list for the different endpoints and one url for the base path of the api.
+players_endpoint    =   'https://api.xfl.com/scoring/v3.30/players'
+teams_endpoint      =   'https://api.xfl.com/scoring/v3.30/teams'
+stats_endpoint      =   'https://api.xfl.com/scoring/v3.30/playerstats'
+games_list_endpoint =   'https://api.xfl.com/scoring/v3.30/games'
 
 # class xfl is the powerhouse of the cell.
 
@@ -30,6 +42,9 @@ class xfl:
         self.token = token
         pass
 
+    def get_stuff(self):
+        pass
+
     # get_players returns a dataframe of all players in the xfl
     # regardless of injury status. The primary key is 'OfficialId'
     def get_players(self):
@@ -44,7 +59,7 @@ class xfl:
             response = json.loads(api_response)
 
             global players
-            players = pd.read_json(response)
+            players = pd.read_json(response).set_index('OfficialId')
 
             # concatenating player first and last name.
             players["Player"] = players['FirstName'].astype(str) + " " + players['LastName']
@@ -77,12 +92,13 @@ class xfl:
             cols.reverse()
             players = players[cols]
 
-            # tada! we have a nice dataframe full of players.
-            print(players)
-            pass
+            # tada! we have a nice dataframe full of player
+            return players
         except requests.exceptions.HTTPError as err:
             raise SystemExit(err)
 
+
+# TODO: These next three functions could use some DRY.
     # get_stats_combined retrieves all overall counting stats.
     # OfficialId is the primary key, and is associated with the same
     # column in get_players() method.
@@ -101,10 +117,9 @@ class xfl:
             response = json.loads(api_response)
 
             global player_stats
-            player_stats = pd.read_json(response).fillna(0)
+            player_stats = pd.read_json(response).fillna(0).set_index('OfficialId')
 
-            print(type(player_stats))
-            pass
+            return player_stats
         except requests.exceptions.HTTPError as err:
             raise SystemExit(err)
         pass
@@ -122,23 +137,60 @@ class xfl:
             response = json.loads(api_response)
 
             global teams
-            teams = pd.read_json(response).fillna(0)
+            teams = pd.read_json(response).fillna(0).set_index('OfficialId')
 
-            print(teams)
+            return teams
+        except requests.exceptions.HTTPError as err:
+            raise SystemExit(err)
+        pass
+
+    # TODO: def get_games():
+
+    def get_games(self):
+        url = games_list_endpoint
+
+        try:
+            r = requests.get(url, params=api_params)
+            r.raise_for_status()
+            api_response = json.dumps(r.text, indent=4, sort_keys=True)
+            response = json.loads(api_response)
+
+            global teams
+            games_list = pd.read_json(response).fillna(0).set_index('OfficialCode')
+
+            return games_list
             pass
         except requests.exceptions.HTTPError as err:
             raise SystemExit(err)
         pass
 
+    def get_gamedata(self):
+        pass
+
+    
+    # TODO: For each game in games, get stats and generate a data frame.
+
+
+# example usage
 
 # x gon' give it to ya.
 x = xfl(token)
 
+# get a list of all xfl players
 players = x.get_players()
-print(players)
+players.to_sql("players", con=engine, if_exists="replace")
 
+# get a list of all xfl teams
 teams = x.get_teams()
-print(teams)
+teams.to_sql("teams", con=engine, if_exists="replace")
 
+# get a table of league wide stats for the seaso
 player_stats = x.get_stats_combined("season")
-print(player_stats)
+player_stats.to_sql("player_stats", con=engine, if_exists="replace")
+
+# print a list of games
+games = x.get_games()
+games.to_sql("games", con=engine, if_exists="replace")
+
+# turn out the lights.
+# the party's over.
